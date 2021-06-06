50f7376f01b5f9fafb4d9134f20a8181052d288e
senjam008 <senjamjobs@gmail.com>
2021-06-06 11:11:53 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -56,7 +56,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered tttttttt by{' '}
+          Powered tttttttttetetetetetetete best by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
